4219ca24a93d3f40c51c3c6dbd6d59fb2bf9bebc
jekon791 <32432548+jekon791@users.noreply.github.com>
2019-06-07 21:28:11 +0300

    up

--- src/board.js
+++ src/board.js
@@ -3,7 +3,8 @@ import { addCard, addBtn, seeOverlayPost} from "./addCard";
 import { seeOverlayPatch, updateCard} from './cardPatch'
 
 //Привязка событий по кликам
-document.querySelector("#conteiner").addEventListener("click", getStart, { once: true });
+document.addEventListener('DOMContentLoaded', getStart(), false);
+//document.querySelector("[data-conteiner]").addEventListener("click", getStart, { once: true });
 document.getElementById("closeFormPost").addEventListener('click',seeOverlayPost)
 document.getElementById("closeFormPatch").addEventListener('click',seeOverlayPatch)
 document.getElementById("goPOST").addEventListener("click", addCard);
